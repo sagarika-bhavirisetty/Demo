e439a7f3614156e3f1675928c0a450f6db3f956a
sagarika-bhavirisetty <110666038+sagarika-bhavirisetty@users.noreply.github.com>
2022-08-08 20:30:35 +0530

    Add files via upload

---
+++ src/Sequence.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+    public class Sequence {
+        public static void main(String[] args)
+        {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            while(n!=1)
+            {
+                if(n%2==0)
+                {
+                    n=n/2;
+                }
+                else
+                {
+                    n=3*n+1;
+                }
+                System.out.println(n);
+            }
+        }
+
+    }
